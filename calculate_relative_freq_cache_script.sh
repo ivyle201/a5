221b75709bbea63fb5846b5383cdca749dfe0933
ivyle201 <vy.l.le@sjsu.edu>
2022-11-19 15:54:42 -0500

    adding missing script output

--- calculate_relative_freq_cache_script.sh
+++ calculate_relative_freq_cache_script.sh
@@ -24,43 +24,43 @@ else
 	exit 1
 fi
 
-echo "Reading hashtag files from directory $INFLUENCE_CLUSTER and writing to $output_file. Processing...wait time: $time."
+echo "Reading hashtag files from directory $INFLUENCE_CLUSTER and writing to $output_file. Processing...please wait $time."
 
 #header columns
 echo -e "hashtag\tcluster_C_leader(userID)\trelative_frequency_H_C\tfrequency_H_in_C\tfrequency_H_overall\tcount_H_in_C\tcount_hashtags_in_C\tcount_H_entire_dataset\tcount_hashtags_entire_dataset" > $output_file
 
 declare -A cache_array
 
-#iterate through the INFL file from INFLUENCE_CLUSTER directory
-for INFL_FILE in `ls $INFLUENCE_CLUSTER | head -n 1`; do
+#iterate through the INFL file from INFLUENCE_CLUSTER directory 
+
+for INFL_FILE in `ls $INFLUENCE_CLUSTER | head -n 1000`; do
+
 	for HASHTAG in `cat $INFLUENCE_CLUSTER/$INFL_FILE` ; do
+        	
 		count_H_in_C=`grep $HASHTAG $INFLUENCE_CLUSTER/$INFL_FILE | wc -l`
 		count_hashtags_in_C=`cat $INFLUENCE_CLUSTER/$INFL_FILE | wc -l`
-
+		
 		#get count_H_entire_dataset
 		count_H_entire_dataset=${cache_array[$HASHTAG]}
 		#if the value is not in the array
 		if [ -z "$count_H_entire_dataset" ] ;
 		then
+		
 			if [ "$value" -eq 1 ] ; 
 			then
 				count_H_entire_dataset=`grep $HASHTAG entire_hashtags_for_replies.tsv | wc -l` 
-
+		
 			elif [ "$value" -eq 2 ] ; 
 			then
 				count_H_entire_dataset=`grep $HASHTAG entire_hashtags_for_retweets.tsv | wc -l`
 			fi
 			cache_array[$HASHTAG]=$count_H_entire_dataset
 		fi
-		echo $HASHTAG
-		#echo $count_hashtags_in_C
-		#echo  $frequency_H_overall
-		#echo $count_hashtags_entire_dataset
-		echo $count_H_entire_dataset
-		frequency_H_in_C=`echo "scale=10; $count_H_in_C/$count_hashtags_in_C" | bc -l`
-		frequency_H_overall=`echo "scale=10; $count_H_entire_dataset/$count_hashtags_entire_dataset" | bc -l`
-		relative_frequency_H_C=`echo "scale=10; $frequency_H_in_C/$frequency_H_overall" | bc -l`
+
+		frequency_H_in_C=`echo "scale=2; $count_H_in_C/$count_hashtags_in_C" | bc -l`
+		frequency_H_overall=`echo "scale=7; $count_H_entire_dataset/$count_hashtags_entire_dataset" | bc -l`
+		relative_frequency_H_C=`echo "scale=2; $frequency_H_in_C/$frequency_H_overall" | bc -l`
+		
 		echo -e "$HASHTAG\t $INFL_FILE\t$relative_frequency_H_C\t$frequency_H_in_C\t$frequency_H_overall\t$count_H_in_C\t$count_hashtags_in_C\t$count_H_entire_dataset\t$count_hashtags_entire_dataset"
 	done;
-done 
-#>> $output_file
+done >> $output_file 
--- create_clusters.sh
+++ create_clusters.sh
@@ -9,11 +9,13 @@ then
 
 	file="INFL_UserIDs_3Rgreater_replies.tsv"
 	INFLUENCE_CLUSTER="INFLUENCE_CLUSTERS_REPLIES"
+	file2="downloaded_tweets_extend_original_nolf2_NOBOT_repliedTo.tsv"
 
 elif [ "$value" -eq 2 ] ;
 then
         file="INFL_UserIDs_3Rgreater_retweets.tsv"
         INFLUENCE_CLUSTER="INFLUENCE_CLUSTERS_RETWEETS"
+	file2="join_retweeted_message_ids_with_original_nolf2_NOBOT.tsv"
 else
 	echo "Wrong number was enter. Please try again."
 	exit 1
@@ -21,7 +23,7 @@ else
 fi	
 for userID_of_INFL in `cat $file` ; do
       		
-	grep  $userID_of_INFL downloaded_tweets_extend_original_nolf2_NOBOT.tsv \
+	grep  $userID_of_INFL $file2 \
 	| awk -F "\t" '{print $4}' | tr ',' '\n' | tr '"' '\n'| tr '[:upper:]' '[:lower:]' \
 	| grep . > $INFLUENCE_CLUSTER/$userID_of_INFL ;
 done
